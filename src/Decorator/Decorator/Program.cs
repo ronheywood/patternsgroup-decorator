486f7c4d8d8d8b00dfcf8c175639f970515d2847
Ron Heywood <ron.heywood@spektrix.com>
2019-06-25 08:50:13 +0100

    First iteration for Kata

---
+++ src/Decorator/Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using StarBuzz.Beverages;
+
+namespace StarBuzz
+{
+    class Program
+    {
+        private static readonly List<Beverage> Menu = new List<Beverage>() { new DarkRoast(), new Espresso(), new HouseBlend(), new Decaf()};
+        static void Main(string[] args)
+        {
+            var storeInstance = new Store(Menu,null);
+            storeInstance.PrintMenu();
+        }
+    }
+}
---
+++ src/Decorator/Decorator/Store.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StarBuzz.Beverages;
+using StarBuzz.Condiments;
+
+namespace StarBuzz
+{
+    public class Store
+    {
+        private readonly List<Beverage> _coffeeMenu;
+        private readonly IStarBuzzConsole _writer;
+        private List<Condiment> _condimentsMenu;
+
+        public Store(List<Beverage> coffeeMenu, List<Condiment> condimentsMenu = null, IStarBuzzConsole writer = null)
+        {
+            _condimentsMenu = condimentsMenu ?? new List<Condiment>();
+            _coffeeMenu = coffeeMenu;
+            _writer = writer ?? new StarBuzzConsole();
+        }
+
+        public void PrintMenu()
+        {
+            _writer.WriteLine("Welcome to StarBuzz coffee!\r\n\r\nMenu:");
+            foreach (var beverage in _coffeeMenu)
+            {
+                _writer.WriteLine($"{beverage} - £{beverage.Cost():F2}");
+            }
+
+            if (_condimentsMenu.Any())
+            {
+                _writer.WriteLine("\r\nCondiments:");
+                foreach (var condiment in _condimentsMenu)
+                {
+                    //{ beverage.Cost():F2}
+                    _writer.WriteLine($"{condiment.GetDescription()} - £0.25");
+                }
+            }
+            _writer.ReadKey();
+        }
+    }
+
+    public class StarBuzzConsole : IStarBuzzConsole
+    {
+        public void WriteLine(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public string ReadKey()
+        {
+            return Console.ReadKey().ToString();
+        }
+    }
+
+    public interface IStarBuzzConsole
+    {
+        void WriteLine(string str);
+        string ReadKey();
+    }
+}
